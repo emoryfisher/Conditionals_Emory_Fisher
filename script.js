65f6dc6a223cca1d3b9a60ccc3c21ff33254cb12
emoryfisher <70530177+emoryfisher@users.noreply.github.com>
2020-09-13 22:19:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var wrapperEle = document.body.querySelector(".wrapper");
+
+var prompter = prompt("Enter a number");
+
+if var number1 {wrapperEle.innerHTML=("Statement is true")};
+  
+  var prompter = prompt("Enter an operator (+-/*)");
+
+  if var number2 {wrapperEle.innerHTML=("Statement is true")};
